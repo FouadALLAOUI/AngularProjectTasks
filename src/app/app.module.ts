dd11c60b6d90671210d99d8fbf6c3c488e06b1d1
FouadALLAOUI <fouadallaoui70@gmail.com>
2023-01-27 19:50:19 +0100

    Add Tasks component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { TasksComponent } from './components/tasks/tasks.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    TasksComponent
   ],
   imports: [
     BrowserModule,
