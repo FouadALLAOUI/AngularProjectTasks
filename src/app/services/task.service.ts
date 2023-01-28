09893457347a2630ee2fdd5abf5b8326cb422666
FouadALLAOUI <fouadallaoui70@gmail.com>
2023-01-28 17:45:02 +0100

    Create getTasks fct & the interface task

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Task } from '../models/task';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +10,7 @@ export class TaskService {
   constructor(private http: HttpClient) { }
 
   findAll(){
-    return this.http.get("htstp://localhost:5000/tasks");
+    return this.http.get<Task[]>("htstp://localhost:5000/tasks");
   }
 
 }
